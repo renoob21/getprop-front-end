8fbbb966419b110c1d734de5bd6657c3e2ddf44c
renoob21 <reno.iqbalsah21@gmail.com>
2025-04-24 12:33:14 +0700

    set base url to api

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const IS_DEF = location.hostname === "127.0.0.1" || location.hostname === "local
 
 const API_BASE_URL = IS_DEF 
                         ? "http://127.0.0.1:3000"
-                        : "http://34.9.92.150:8080";
+                        : "https://api.senengoding.com";
 
 
 window.CONFIG = {
